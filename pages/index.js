118cf23b94dd7fb21725a0cc6550c3e50339eada
Guilherme Toja <guilherme.toja.camilo@gmail.com>
2024-03-19 13:13:06 +0000

    atualiza o arquivo index.js para ir conforme o .editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Maria, eu te amo muito, obrigado por tudo. Se você me ama, da uma risadinha! Testando o continuous deployment</h1>
+  return <h1>Maria, eu te amo muito, obrigado por tudo. Se você me ama, da uma risadinha! Testando o continuous deployment</h1>
 }
 
 export default Home;
