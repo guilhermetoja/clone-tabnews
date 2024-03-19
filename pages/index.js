40a1b29b8d53736a9a21f6e572f0c65722ef9cea
Guilherme Toja <guilherme.toja.camilo@gmail.com>
2024-03-19 13:40:39 +0000

    prettier e editorconfig para deixar tudo padronizado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-  return <h1>Maria, eu te amo muito, obrigado por tudo. Se você me ama, da uma risadinha! Testando o continuous deployment</h1>
+function Home() {
+  return (
+    <h1>
+      Maria, eu te amo muito, obrigado por tudo. Se você me ama, da uma
+      risadinha! Testando o continuous deployment
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("identacao errada");
+}
 export default Home;
