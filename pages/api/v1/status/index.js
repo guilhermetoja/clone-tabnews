b3a8f3397b2d19d814ad0f33aa92a45e47c2fb14
Guilherme Toja <guilherme.toja.camilo@gmail.com>
2024-04-03 17:42:31 +0000

    testing api with continous deployment

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(request, response) {
-  response.status(200).json({ chave: "Valor", amor: "Maria" });
+  response.status(200).json({ chave: "Valor", amor: "Maria", brother: "Leo" });
 }
 
 export default status;
